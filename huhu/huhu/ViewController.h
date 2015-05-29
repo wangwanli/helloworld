bbebdf93304e0ec8b16efcff01910d8a86743a63
wangwanli <a13340118369@163.com>
2015-05-29 13:20:07 +0800

    juj

---
+++ huhu/huhu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  huhu
+//
+//  Created by wangwanli on 15/5/29.
+//  Copyright (c) 2015年 wangwanli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ huhu/huhu/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  huhu
+//
+//  Created by wangwanli on 15/5/29.
+//  Copyright (c) 2015年 wangwanli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"helloWorld");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
