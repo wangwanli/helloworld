cda0f1fe68de3d050562cb45e3a11a2b1de57c65
wangwanli <a13340118369@163.com>
2015-05-29 13:25:36 +0800

    huhu1

--- huhu/huhu/ViewController.m
+++ huhu/huhu/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"helloWorld");
+    NSLog(@"helloWorld111");
 }
 
 - (void)didReceiveMemoryWarning {
